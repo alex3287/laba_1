6b05778dacc9bbe6eb090ea6c4e4b41a66fcce8b
Alexander Mangazeev <alex3287@bk.ru>
2020-11-25 01:38:07 +0700

    test QFileInfo

--- laba_1/main.cpp
+++ laba_1/main.cpp
@@ -1,6 +1,7 @@
 #include <QCoreApplication>
 #include <QTextStream>
 #include <QString>
+#include <QFileInfo>
 
 int main(int argc, char *argv[])
 {
@@ -14,8 +15,18 @@ int main(int argc, char *argv[])
 
     //test comand line
     if (argc > 1){
+        // file name it is arg
         QString f1 = argv[1];
         out<<f1<<endl;
+        // check correct file
+        if (!QFile(f1).exists()) {
+            qWarning("The file does not exist");
+            return 1;
+          }
+        QFileInfo fileinfo(f1);
+        qint64 size = fileinfo.size();
+        QString str = "The size is: %1 bytes";
+        out << str.arg(size) << endl;
     }
     if (argc > 2){
         QString f2 = argv[2];
