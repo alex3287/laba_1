0875772e1efb4881460f3014787a7eaef81dfc98
Alexander Mangazeev <alex3287@bk.ru>
2020-11-24 23:32:45 +0700

    test comand line and QString

--- laba_1/main.cpp
+++ laba_1/main.cpp
@@ -8,8 +8,19 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     QTextStream out(stdout);
 
+    // test QString
     QString s = "Hello world!";
-    out<<s << endl;
+    out<<s << "  "<< endl;
+
+    //test comand line
+    if (argc > 1){
+        QString f1 = argv[1];
+        out<<f1<<endl;
+    }
+    if (argc > 2){
+        QString f2 = argv[2];
+        out<<f2<<endl;
+    }
 
     return a.exec();
 }
